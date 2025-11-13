f3afa5ca9c0dc95a101273c97ad15174ca7af595
João Paulo <jpborella1@gmail.com>
2025-11-13 19:27:51 -0300

    atualizar referências de imagem para usar BASE_URL e adicionar arquivo vite-env.d.ts

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { data } from '../data'
 export default function Hero() {
     return (
         <section className="flex flex-col items-center text-center py-5 ">
-            <img src="/images/foto-maiara.png"
+            <img src={`${import.meta.env.BASE_URL}images/foto-maiara.png`}
                 alt={data.vendedora.nome}
                 className="w-48 h-48 object-cover object-top rounded-full shadow-xl" />
             <h1 className="text-4xl md:text-5xl font-bold mb-4 ">{data.vendedora.nome}</h1>
