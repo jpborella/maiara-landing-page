1eaa4d91b3debb3afa8b91212377d7bda8b42297
João Paulo <jpborella1@gmail.com>
2025-11-13 13:49:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Landing Page - Maiara Ferraz 
+
+Esta landing page é baseada no projeto [andre-landing-page](https://github.com/jpborella/andre-landing-page), com alterações apenas nas informações dos vendedores.
---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import { data } from '../data'
+
+export default function Hero() {
+    return (
+        <section className="flex flex-col items-center text-center py-5 ">
+            <img src="/images/foto-maiara.png"
+                alt={data.vendedora.nome}
+                className="w-48 h-48 object-cover object-top rounded-full shadow-xl" />
+            <h1 className="text-4xl md:text-5xl font-bold mb-4 ">{data.vendedora.nome}</h1>
+            <p className="text-xl md:text-2xl font-medium text-gray-600">{data.vendedora.cargo}</p>
+        </section>
+    )
+}
